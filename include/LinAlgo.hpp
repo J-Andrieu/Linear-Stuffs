d59ab360caabddd1ebdf1357d132250811021b3d
Jordan Andrieu <jandrieu@nevada.unr.edu>
2019-08-27 16:02:54 -0700

    InitGPU() favors gpu, will default to cpu if gpu is not available... the function name is sorta a lie now... i guess. Meh, the defaulting really shouldn't end up happening

--- include/LinAlgo.hpp
+++ include/LinAlgo.hpp
@@ -208,17 +208,26 @@ static cl_int LinAlgo::InitGPU() {
 	int platform_index = choosePlatform(m_platform_id, ret_num_platforms, 2.2);
 	OPENCL_VERSION = getPlatformVersion(m_platform_id[platform_index]);
 	printf("The chosen platform version is %f\n", OPENCL_VERSION);
-	ret = clGetDeviceIDs(m_platform_id[platform_index], CL_DEVICE_TYPE_GPU, 1, &m_device_id, &ret_num_devices);
+	if (OPENCL_VERSION < 2.0) {
+        printf("Warning: Opencl 1.x functions are not required to be implemented,\nFunctions such as clCreateQueue() which only changed names between versions may cause issues.\n");
+	}
+	ret = clGetDeviceIDs(m_platform_id[platform_index], CL_DEVICE_TYPE_GPU, 0, &m_device_id, &ret_num_devices);
+	if (ret_num_devices == 0) {
+        ret = clGetDeviceIDs(m_platform_id[platform_index], CL_DEVICE_TYPE_CPU, 0, &m_device_id, &ret_num_devices);
+        if (ret_num_devices == 0) {
+            printf("Warning: No OpenCL device available\n");
+            return ret;
+        } else {
+            ret = clGetDeviceIDs(m_platform_id[platform_index], CL_DEVICE_TYPE_CPU, 1, &m_device_id, &ret_num_devices);
+        }
+	} else {
+        ret = clGetDeviceIDs(m_platform_id[platform_index], CL_DEVICE_TYPE_GPU, 1, &m_device_id, &ret_num_devices);
+	}
+
 	if (ret != CL_SUCCESS) {
 		printf("Could not get device IDs.\n");
 		return ret;
 	}
-	if (ret_num_devices == 0) {
-        printf("No GPU located.\n");
-        return ret;
-	}
-
-
 
 	//create the context
 	m_context = clCreateContext(NULL, 1, &m_device_id, NULL, NULL, &ret);
